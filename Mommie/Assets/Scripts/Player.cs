f5c8b7790085d20c3f437ecfa09d593d6639bfaf
Unknown <e.escher@rubika-edu.com>
2018-01-27 16:19:25 +0100

    en cours de merge

--- Mommie/Assets/Scripts/Player.cs
+++ Mommie/Assets/Scripts/Player.cs
@@ -2,99 +2,131 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.PostProcessing;
 
 public class Player : MonoBehaviour {
 
-	public float speedMouvement;
-	public float maxSpeedMouvement;
-	public float cadenceDeTir = 1;
-	bool isShooting = false;
-	public GameObject burst;
-    public GameObject machineGunBurst;
-    public bool isMoving =false;
-    public float recul;
-    public int score = 0;
-    public bool usingMachineGun = false;
-    bool canMove = true;
-    bool canSwitch = true;
-    ParticleSystem part;
-    AudioSource audioS;
-    SpriteRenderer sprite;
-    Rigidbody2D rb;
-    Animator anim;
+	Rigidbody2D rb;
+	public float speedMouvement=100;
+	public float maxSpeedMouvement=50;
+	public float drag=10;
+
+	public float timeLeft = 3;
+	float timeLeftInitial;
+	public float delayBeforeDeath = 1;
+	bool canMove = true;
+	float initalspeed;
+	private bool isMoving = false;
+	private Vector2 direction;
+	public GameObject coeur;
+	public float LancerDeCoeur = 15;
+	private bool canSend = true;
+    public SmoothCamera2D camera;
+    public Transform viseur;
+    public float delayLancer = 1;
+	ParticleSystem particle; 
+	public PostProcessingProfile initial;
+	public PostProcessingProfile shoot;
+	public float cdShoot =1f;
+	public GameObject particuleRespawn;
+	public GameObject RespawnButton;
+	public GameObject DeadBody;
+	SpriteRenderer sprite;
+	Animator anim;
+	bool canSpawnButton = true;
+	Vector3 positionDeadBody;
+	public float timeZoom =0.3f;
+
+	void Awake(){
+		direction = new Vector2 (1, 1);
+	}
 
 	void Start () {
-		sprite = GetComponent<SpriteRenderer> ();
-		rb = GetComponent<Rigidbody2D> ();
+
+		rb = GetComponent<Rigidbody2D>();
 		anim = GetComponent<Animator> ();
-		part = transform.GetChild (0).GetComponent<ParticleSystem> ();
-        audioS = GetComponent<AudioSource>();
+		sprite = GetComponent<SpriteRenderer> ();
+		rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+		rb.drag = drag;
+		rb.gravityScale = 0;
+		particle = transform.GetChild (4).GetComponent<ParticleSystem> ();
+		timeLeftInitial = timeLeft;
+		GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Animator> ().SetBool ("Dezoom", false);
 	}
 
-	void Update () {
-		CheckMaxVelocity();
-		if ((Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) && canMove) {
-			isMoving = true;
-			anim.SetBool ("moving", true);
+	void Update()
+	{
+		timeLeft -= Time.deltaTime;
+		if( Input.GetAxisRaw ("Fire1")>0 && canSend){
+            StartCoroutine(PrepareLancer());
+            canSend = false;
+            canMove = false;
+		}
+		if ( timeLeft <= 1 )
+		{
+			canMove = false;
+			if (canSend) {
+				StartCoroutine (PrepareLancer());
+				canSend = false;
+			}
+			StartCoroutine (Die());
+		}
+	}
 
-            //Deplace le joueur
-			rb.AddForce (new Vector2 (Input.GetAxisRaw ("Horizontal") * speedMouvement, Input.GetAxisRaw ("Vertical") * speedMouvement));
+	void FixedUpdate() {
+		CheckMaxVelocity ();
+		//Deplacement
+        if (canMove) {
+			float tempX = Input.GetAxisRaw ("Horizontal"), tempY = Input.GetAxisRaw ("Vertical");
+			if (tempX != 0 || tempY != 0) {
+				direction.Set (tempX, tempY);
+				rb.AddForce((direction * speedMouvement * Time.deltaTime), ForceMode2D.Force);
+				viseur.localPosition = direction * 40;
+                isMoving = true;
+			} else {
+				isMoving = false;
+			}
 
-            //Quand le joueur bouge, on emet de la poussiere a ses pieds
-			var emission = part.emission;
-			emission.rateOverTime = 20;
+			if (tempY > 0) {
+				anim.SetBool ("Up", true);
+				if (tempX < 0) {
+					sprite.flipX = false;
+				} else if (tempX > 0) {
+					sprite.flipX = true;
+				}
+			} else if (tempY < 0){
+				anim.SetBool ("Up", false);
+				if (tempX < 0) {
+					sprite.flipX = true;
+				} else if (tempX > 0) {
+					sprite.flipX = false;
+				}
+			}
 
-            //Oriente le joueur en fonction des inputs
+			anim.SetBool ("Running", isMoving);
+			if (isMoving) {
+				var emission = particle.emission;
+				emission.rateOverTime = 60;
+			} else {
 
-            if (Input.GetAxisRaw("Horizontal") < 0)
-            {
-                    sprite.flipX = true;
-            }
-            else if (Input.GetAxisRaw("Horizontal") > 0)
-            {
-                    sprite.flipX = false;
-            }
-		} else {
-			isMoving = false;
-			anim.SetBool ("moving", false);
+				var emission = particle.emission;
+				emission.rateOverTime = 0;
 
-            //Si il ne bouge pas, pas de trainé de poussiere
-			var emission = part.emission;
-			if (!isShooting)
-                emission.rateOverTime = 0;
+			}
 		}
 
-		if (Input.GetAxisRaw ("Fire1") != 0 && !isShooting) {
-			StartCoroutine (Shoot ());
-            if (!usingMachineGun)
-                anim.SetBool("shoot", true);
-            audioS.pitch = Random.Range(0.9f, 1.1f);
-            audioS.Play();
-            if (!usingMachineGun)
-                GameObject.Find("MainCamera").GetComponent<CameraShake>().shakeDuration = 0.7f;
-            else
-                GameObject.Find("MainCamera").GetComponent<CameraShake>().shakeDuration = 0.2f;
-            if (!usingMachineGun)
-                StartCoroutine(Recul());
-        }
-
-        if (usingMachineGun)
-            cadenceDeTir = 0.05f;
         else
-            cadenceDeTir = 1;
-        if (Input.GetAxisRaw("Fire2") != 0 && canSwitch)
-            StartCoroutine(SwitchWeapons());
-        //Retour au menu
-        if(Input.GetAxisRaw("Cancel") != 0)
         {
-            SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+            float tempX = Input.GetAxisRaw("Horizontal"), tempY = Input.GetAxisRaw("Vertical");
+            if (tempX != 0 || tempY != 0)
+            {
+                direction.Set(tempX, tempY);
+                viseur.localPosition = direction * 40;
+            }
         }
 	}
-
-    //Cette methode limite la vitesse maximal du joueur
 	void CheckMaxVelocity()
 	{
-		//à optimiser
 		if (rb.velocity.x > maxSpeedMouvement)
 			rb.velocity = new Vector2(maxSpeedMouvement, rb.velocity.y);
 		if (rb.velocity.x < -maxSpeedMouvement)
@@ -105,61 +137,70 @@ public class Player : MonoBehaviour {
 			rb.velocity = new Vector2(rb.velocity.x, -maxSpeedMouvement);
 	}
 
-    IEnumerator SwitchWeapons()
-    {
-        canSwitch = false;
-        usingMachineGun = !usingMachineGun;
-        yield return new WaitForSeconds(1);
-        canSwitch = true;
-    }
-
-    IEnumerator Recul()
-    {
-        if (!sprite.flipX)
-            rb.AddForce(Vector2.left * recul);
-        else
-            rb.AddForce(Vector2.right * recul);
-
-        //Emet quelques particules de poussière à nos pieds
-        var emission = part.emission;
-        emission.rateOverTime = 20;
-        yield return new WaitForSeconds(0.3f);
-        emission.rateOverTime = 0;
-
-    }
-
-	IEnumerator Shoot()
+	IEnumerator Die()
 	{
-        if (!usingMachineGun)
-            StartCoroutine(CanMoveAgain());
-		isShooting = true;
-		Quaternion rot;
-
-        //Oriente le tir en fonction de l'orientation du joueur
-		if (sprite.flipX == false) {
-			rot = Quaternion.identity * Quaternion.Euler (0, 0, -22.5f);
-		} else {
-			rot = Quaternion.identity * Quaternion.Euler (0, 0, -22.5f) * Quaternion.Euler(0,0,170);
-		}
-        GameObject shoot = new GameObject();
-        if (!usingMachineGun)
-            shoot = Instantiate (burst, transform.position - new Vector3(0,0,1), rot);
-        else
-            shoot = Instantiate(machineGunBurst, transform.position - new Vector3(0, 0, 1), rot);
-        shoot.transform.parent = transform;
-		Destroy (shoot, 2);
-
-
-		yield return new WaitForSeconds (cadenceDeTir);
-		isShooting = false;
+		yield return new WaitForSeconds (delayBeforeDeath/2);	
+		anim.SetBool ("Dying", true);
 	}
 
-    //Micro délaie de stun après un tir
-    IEnumerator CanMoveAgain()
+	public void Menu()
+	{
+		if (canSpawnButton) {
+			GameObject button = Instantiate (RespawnButton, transform.position, Quaternion.identity);
+			button.transform.parent = GameObject.Find ("Canvas").transform;
+			button.transform.localPosition = Vector3.zero;
+			button.transform.localScale = new Vector3 (1, 1, 1);
+			canSpawnButton = false;
+		}
+	}
+
+    private IEnumerator PrepareLancer()
     {
-        canMove = false;
-        yield return new WaitForSeconds(0.2f);
-        anim.SetBool("shoot", false);
-        canMove = true;
+		GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<PostProcessingBehaviour> ().profile = shoot;
+		positionDeadBody = transform.position;
+
+		GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Animator> ().SetBool ("Dezoom", true);
+
+		anim.SetBool ("Shooting", true);
+		var emission = particle.emission;
+		emission.rateOverTime = 0;
+
+        yield return new WaitForSeconds(delayLancer);
+        Rigidbody2D tempCoeur = Instantiate(coeur, transform.position + new Vector3(direction.x, direction.y), Quaternion.identity).GetComponent<Rigidbody2D>();
+        tempCoeur.AddForce(direction * LancerDeCoeur, ForceMode2D.Impulse);
+        camera.target = tempCoeur.transform;
+
     }
+
+    public void ReInitSendHeart()
+    {
+		GameObject deadBro = Instantiate (DeadBody, positionDeadBody, Quaternion.identity);
+		Destroy (deadBro, 3);
+
+
+		GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Animator> ().SetBool ("Dezoom",false);
+
+		StartCoroutine (WaitBeforeCanShootAgain ());
+        canMove = true;
+		timeLeft = timeLeftInitial;
+		anim.SetBool ("Restart", true);
+		StartCoroutine (RestartBackToFalse ());
+		GameObject camembert= Instantiate (particuleRespawn, transform.position+new Vector3(0,0,10), Quaternion.identity);
+		Destroy (camembert, 3);
+		GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<PostProcessingBehaviour> ().profile = initial;
+    }
+
+	IEnumerator WaitBeforeCanShootAgain()
+	{
+		yield return new WaitForSeconds (cdShoot);
+		canSend = true;
+	}
+
+	IEnumerator RestartBackToFalse()
+	{
+
+		yield return new WaitForSeconds(0.1f);
+		anim.SetBool ("Restart", false);
+		anim.SetBool ("Shooting", false);
+	}
 }
